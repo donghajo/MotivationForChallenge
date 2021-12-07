fbb0a4a04efabce752aefdef1037388cc25c0470
jodongha <66076070+jodongha@users.noreply.github.com>
2021-12-08 00:41:14 +0900

    database 맞게 속성명 수정

--- src/routes/challengeRouter.js
+++ src/routes/challengeRouter.js
@@ -2,9 +2,9 @@ var router = require('express').Router();
 const challengeController = require('../controllers/challengeController');
 
 //get page
-router.get('/list/:category_id', challengeController.getList);
-router.get('/list/insert/:category_id', challengeController.getInsertChallenge);
-router.get('/challenge/:id', challengeController.getDetail);
+router.get('/list/:challenge_type', challengeController.getList);
+router.get('/list/insert/:challenge_type', challengeController.getInsertChallenge);
+router.get('/challenge/:challenge_uid', challengeController.getDetail);
 
 //insert 
 router.post('/create', challengeController.createChallenge);
