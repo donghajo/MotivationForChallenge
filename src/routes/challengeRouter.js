6d3a325967d94e13529af378e19d561fc289f4d9
jodongha <66076070+jodongha@users.noreply.github.com>
2021-12-09 18:55:59 +0900

    image upload and print

--- src/routes/challengeRouter.js
+++ src/routes/challengeRouter.js
@@ -1,5 +1,22 @@
 var router = require('express').Router();
 const challengeController = require('../controllers/challengeController');
+var multer = require('multer');
+var path = require('path');
+
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, "uploads/");
+    },
+    filename: function(req, file, cb){
+        const ext = path.extname(file.originalname);
+        cb(null, path.basename(file.originalname, ext)+"-"+Date.now()+ext);
+    },
+});
+var upload = multer({storage:storage});
+
+
+
 
 //get page
 router.get('/list/:challenge_type', challengeController.getList);
@@ -7,7 +24,8 @@ router.get('/list/insert/:challenge_type', challengeController.getInsertChalleng
 router.get('/challenge/:challenge_uid', challengeController.getDetail);
 
 //insert 
-router.post('/create', challengeController.createChallenge);
+router.post('/create', upload.single('image'), challengeController.createChallenge);
+
 
 
 
