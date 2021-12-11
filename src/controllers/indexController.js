78e6467870cd2be75c90c96a22ea67ba769b5621
jodongha <66076070+jodongha@users.noreply.github.com>
2021-12-11 22:32:44 +0900

    main page / .gitignore  수정

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,11 +1,16 @@
 const indexService = require('../services/indexService');
-
+const challengeService = require('../services/challengeService');
 //get page
 exports.getMain = async(req, res) =>{
     try{
+        const challenge = [];
         const session = req.session.user_uid;
-        const challenge = await indexService.getMyChallenge([session]); 
-        console.log(challenge.length);  
+        const challenger = await indexService.getMyChallenge([session]);
+        for(var i = 0; i < challenger.length; i++){
+            challenge.push(await challengeService.getChallenge([challenger[i].challenge_uid]));
+        }
+        
+        console.log(challenge);  
         return res.render('index',{
             session:session,
             pages: 'category',
